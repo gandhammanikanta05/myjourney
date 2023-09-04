33d9121fb4a604fb315445c2c29dcffb3a12ce88
manikantagandham05 <gandham.manikanta777@gmail.com>
2023-09-04 17:10:43 +0530

    4th commit

--- src/Components/NewAccount/index.js
+++ src/Components/NewAccount/index.js
@@ -1,20 +1,24 @@
 import { Link } from 'react-router-dom'
 import {LoginContainer, MainHeading, CreateHead, LoginForm, UsernameLabel, UsernameInput, PasswordLabel, PasswordInput, LoginBtn, Para} from './styledComponents'
-import { useRef } from 'react'
-import Axios from 'axios';
-import axios from 'axios';
+import { useState } from 'react'
 
 const LoginPage = () => {
-    const username = useRef()
-    const password = useRef()
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const changeUsername = event => {
+        setUsername(event.target.value)
+    }
+
+    const changePassword = event => {
+        setPassword(event.target.value)
+    }
 
     const onsubmitDetails = async event => {
         event.preventDefault()
-        const userName = username.current.value
-        const passWord = password.current.value
-        const userDetails = {userName, passWord}
+        const userDetails = {username, password}
         console.log(userDetails)
-        const url = 'http://localhost:3000/NewAccount'
+        const url = 'http://localhost:4000/NewAccount'
         const options = {
           method: 'POST',
           body: JSON.stringify(userDetails),
@@ -30,9 +34,9 @@ const LoginPage = () => {
             <CreateHead>CREATE AN ACCOUNT</CreateHead>
             <LoginForm onSubmit={onsubmitDetails}>
                 <UsernameLabel htmlFor="username">NAME</UsernameLabel>
-                <UsernameInput id="username" type="text" placeholder="Enter Your Name" ref={username} />
+                <UsernameInput id="username" type="text" placeholder="Enter Your Name" onChange={changeUsername} />
                 <PasswordLabel htmlFor="password">PASSWORD</PasswordLabel>
-                <PasswordInput id="password" type="password" placeholder="Enter Your Password" ref={password} />
+                <PasswordInput id="password" type="password" placeholder="Enter Your Password" onChange={changePassword} />
                 <LoginBtn type="submit">Submit</LoginBtn>
                 <Para>Already have an account ?</Para>
                 <Link to="/LoginPage">
